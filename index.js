becdae8b2472d4486356ab3db1dcd6a1f854c9a1
Lucas Holmquist <lholmqui@redhat.com>
2015-03-25 09:40:58 -0400

    add phishin-js blueprint

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-phishin-js'
+  name: 'ember-cli-phishin-js',
+
+  included: function (app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/phishin-js/dist/phishin-js.js');
+  }
 };
